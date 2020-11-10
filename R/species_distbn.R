92b40450529d75be1365d7b9b530527497f47571
Raahil Madhok <raahil_madhok@hks.harvard.edu>
2020-11-09 19:59:14 -0800

    before submission

--- R/species_distbn.R
+++ R/species_distbn.R
@@ -8,6 +8,7 @@ rm(list=ls())
 require(tidyverse)
 require(fastDummies)
 require(data.table)
+require(stargazer)
 source('/Users/rmadhok/Dropbox (Personal)/ebird_lockdown/scripts/R/select_functions.R')
 setwd('/Users/rmadhok/Dropbox (Personal)/ebird_lockdown/')
 
@@ -79,23 +80,28 @@ m_sp <- sp_dist %>%
   summarize(freq = mean(prop, na.rm=T),
             n_days = first(n_days)) %>%
   group_by(COUNTY, species) %>%
-  mutate(keep = ifelse(YEAR == 2020 & prepost=='POST' & n_days >= 7, 1,0), # dummy if species observed > twice post-lockdown 
+  mutate(keep = ifelse(YEAR == 2020 & prepost=='POST' & n_days >= 7, 1,0), # dummy if species observed >= 7 times post-lockdown 
          keep = max(keep)) %>%
-  filter(keep == 1) %>% select(-c(keep, n_days)) %>% # keep if species observed > twice post-lockdown
+  filter(keep == 1) %>% select(-c(keep, n_days)) %>% # keep if species observed >= 7 times post-lockdown
   group_by(COUNTY, species, YEAR, prepost) %>%
   pivot_wider(names_from = c('YEAR', 'prepost'), values_from = freq) %>%
-  filter((`2020_POST` > `2019_POST`) & (`2020_PRE` <= `2019_PRE`)) # keep if species observed more in 2020 vs. 2019
+  filter((`2020_POST` > `2019_POST`) & (`2020_PRE` <= `2019_PRE`)) %>% # keep if species observed more in 2020 vs. 2019
+  select(COUNTY, species)
 
 # Get daily distribution of marginal species
 sp_dist <- merge(sp_dist, m_sp, by = c('COUNTY', 'species'))
 sp_dist <- arrange(sp_dist, COUNTY, species, YEAR, dif)
 
-# MAIN TEXT ----------------------------------
-sp_bangalore <- c('Black-rumped Flameback', 'Black-crowned Night-Heron', 'Black-headed Ibis', 'Indian Scops-Owl')
-sp_delhi <- c('Black-rumped Flameback', 'Greater Coucal', 'Large-billed Crow', 'Coppersmith Barbet')
+# ----------------------------------------------------
+# SPECIES DISTRIBUTIONS 
+# ----------------------------------------------------
+
+# MAIN TEXT
+sp_blr <- c('Black-rumped Flameback', 'Black-crowned Night-Heron', 'Black-headed Ibis', 'Indian Scops-Owl')
+sp_delhi <- c('Black-rumped Flameback', 'Alexandrine Parakeet', 'Large-billed Crow', 'Coppersmith Barbet')
 sp_bl <- sp_dist %>%
-  filter(COUNTY == 'Bangalore' & species %in% sp_bangalore) %>%
-  mutate(species = factor(species, levels=sp_bangalore))
+  filter(COUNTY == 'Bangalore' & species %in% sp_blr) %>%
+  mutate(species = factor(species, levels=sp_blr))
 sp_dl <- sp_dist %>%
   filter(COUNTY == 'Delhi' & species %in% sp_delhi) %>%
   mutate(species = factor(species, levels=sp_delhi))
@@ -122,9 +128,9 @@ ggplot(sp_main,
   facet_wrap(~COUNTY+species, scales='free', ncol=4)
 ggsave('./figs/species_dist_main.png', height=5,width=15, unit='in')
 
-# APPENDIX  ----------------------------------
+# APPENDIX  
 sp_bl <- sp_dist %>%
-  filter(COUNTY == 'Bangalore' & !(species %in% sp_bangalore) & species!='bird sp.')
+  filter(COUNTY == 'Bangalore' & !(species %in% sp_blr) & species!='bird sp.')
 sp_dl <- sp_dist %>%
   filter(COUNTY == 'Delhi' & !(species %in% sp_delhi))
 sp_apx <- rbind(sp_bl, sp_dl)
@@ -147,3 +153,64 @@ ggplot(sp_apx,
         strip.text = element_text(size=15)) +
   facet_wrap(~COUNTY+species, scales='free', ncol=4)
 ggsave('./figs/species_dist_apx.pdf', height=25,width=20, unit='in')
+
+#------------------------------------
+# MARGINAL SPECIES STATISTICS
+#------------------------------------
+rm(list=c('sp_main', 'sp_apx', 'sp'))
+
+# % of Checklists reporting each marginal species
+citylist <- c('Bangalore', 'Delhi')
+sp_dist <- list()
+for(i in 1:length(citylist)){
+  sp <- sp_list %>%
+    filter(COUNTY == citylist[i]) %>%
+    fastDummies::dummy_cols(select_columns='COMMON.NAME') %>%
+    group_by(COUNTY, YEAR) %>%
+    summarize_at(vars(starts_with('COMMON.NAME_')), sum, na.rm=T) %>% # daily num. of checklists observing species
+    pivot_longer(starts_with('COMMON.NAME_'), 
+                 names_to = 'species', 
+                 values_to = 'number', 
+                 names_prefix='COMMON.NAME_')
+  sp_dist[[i]] <- sp
+}
+sp_dist <- dplyr::bind_rows(sp_dist)
+
+# Merge w num trips/year 
+n_trips <- sample %>%
+  group_by(COUNTY, YEAR) %>%
+  summarize(n_trips = n())
+sp_dist <- merge(sp_dist, n_trips, by=c('COUNTY', 'YEAR'))
+
+# Marginal Species
+m_stats <- sp_dist %>%
+  mutate(prop = round((number/n_trips)*100, 2)) %>% # Prop of checklists reporting species
+  inner_join(m_sp, by=c('COUNTY', 'species')) %>%
+  filter(YEAR == 2019 & species!='bird sp.') %>%
+  select('COUNTY', 'species', 'prop') %>%
+  arrange(COUNTY, desc(prop)) %>%
+  mutate(sp_clean = tolower(species),
+         sp_clean = gsub('-', '', sp_clean))
+
+# Merge IUCN Category
+iucn <- read.csv('./data/india_birdlife.csv') %>%
+  rename(species = English.name, iucn = Global.IUCN.Red.List.Category) %>%
+  select(species, iucn) %>%
+  mutate(sp_clean = tolower(species),
+         sp_clean = gsub('-', '', sp_clean),
+         iucn = as.character(iucn))
+
+m_iucn <- left_join(m_stats, iucn[, c('sp_clean', 'iucn')], by='sp_clean') %>%
+  select(-sp_clean) %>%
+  mutate(iucn = replace(iucn, species=='Asian Koel', 'LC'),
+         iucn = replace(iucn, species=='White-throated Kingfisher', 'LC'),
+         iucn = replace(iucn, species=='Rock Pigeon', 'LC'),
+         iucn = replace(iucn, species=='Blue-throated Flycatcher', 'LC'),
+         iucn = replace(iucn, species=='Gray Heron', 'LC')) %>%
+  group_by(COUNTY) %>%
+  mutate(Rarity = ifelse(prop < quantile(prop, 0.25), 'rare', 'common'))
+
+# Write
+stargazer(m_iucn[m_iucn$COUNTY=='Bangalore',], summary=F, rownames=F, digits=2, out= './tables/m_sp_blr.tex')
+stargazer(m_iucn[m_iucn$COUNTY=='Delhi',], summary=F, rownames=F, digits=2, out= './tables/m_sp_dl.tex')
+
